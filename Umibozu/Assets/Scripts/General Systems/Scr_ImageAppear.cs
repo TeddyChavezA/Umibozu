68fe1df85c6b7a4d0dbed41db7f34f08bd1188db
JayHrib <JayHrib@live.se>
2018-02-07 13:21:17 +0100

    Update
    Updated projectile script to destroy projectiles after 5 sec. Added script to be attached to power ups that displays a introduction to said power ups. Added silhouettes to certain objects for testing purposes

---
+++ Umibozu/Assets/Scripts/General Systems/Scr_ImageAppear.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_ImageAppear : MonoBehaviour {
+
+    [SerializeField] private Image customImage;
+    private bool toContinue = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown && customImage.enabled)
+        {
+            customImage.enabled = false;
+            toContinue = true;
+        }
+
+        if (toContinue)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Umibozu/Assets/Scripts/General Systems/Scr_MoveProjectile.cs
+++ Umibozu/Assets/Scripts/General Systems/Scr_MoveProjectile.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class Scr_MoveProjectile : MonoBehaviour {
 
+    private float destroyTimer = 5f;
+    private float timeToDestroy = 0f;
 
     private void Start()
     {
         Physics2D.IgnoreCollision(GetComponent<Collider2D>(), GameObject.FindWithTag("Player").GetComponent<Collider2D>());
+    }
+
+    void Update()
+    {
+        if (destroyTimer > timeToDestroy)
+        {
+            destroyTimer -= Time.deltaTime;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     void OnBecameInvisible()
